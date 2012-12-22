f181b3be9d1e09745594b8f85699a73f6cdb443a
Markus Alind <markus.alind@gmail.com>
2012-12-22 23:17:18 +0100

    bam script, imm-directory for objects, debug and optimized. Not pretty now.

--- bam.lua
+++ bam.lua
@@ -2,4 +2,17 @@ settings = NewSettings()
 
 settings.cc.includes:Add("include")
 src = Collect("src/*.c")
-bam = Link(settings, "memsearch", Compile(settings, src))
+
+settings.cc.Output = function(settings, input)
+    return 'build/' .. settings.config_name .. '/' .. PathBase(input) .. settings.config_ext
+end
+
+settings.config_name = 'dbg'
+
+bam_dbg = Link(settings, "memsearch" .. '_' .. settings.config_name, Compile(settings, src))
+
+settings.optimize = 1
+settings.config_name = 'opt'
+
+bam_opt = Link(settings, "memsearch" .. '_' .. settings.config_name, Compile(settings, src))
+
